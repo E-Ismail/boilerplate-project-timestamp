ef90af371125acc7e3d49b9829f3cbe14cdbd1c9
echcherrate.ismail <echcherrate.ismail@gmail.com>
2021-06-05 15:10:20 +0000

    Invalid Date error added

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,7 +21,7 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  return res.json({greeting: 'hello API'});
+  return res.json({ greeting: 'hello API' });
 });
 
 
@@ -35,15 +35,20 @@ var listener = app.listen(process.env.PORT, function () {
 app.get('/api/:date?', function (req, res) {
   let date_input = req.params.date;
   let date = new Date();
-  
+
   if (isNumeric(date_input)) {
     date_input = parseInt(date_input);
+    console.log(date_input)
   }
-  
+
   if (date_input !== undefined) {
     date = new Date(date_input);
+    // console.log(date)
+    if (date.toString() === 'Invalid Date') {
+      return res.json({ 'error': 'Invalid Date' });
+    }
   }
-  
+
   return res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
